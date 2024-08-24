9cedf5a20fca07ee17f816ae4a25c25474d654e6
Kyaw Oo <kyawkyawjek@gmail.com>
2024-08-24 11:00:35 +0700

    chore: Update vendor component with table and action buttons

--- src/app/pages/vendor/vendor.component.ts
+++ src/app/pages/vendor/vendor.component.ts
@@ -1,12 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
+import { TTableColumnDef } from '@models/index';
+import { VendorService } from '@services/vendor/vendor.service';
+import { MainLayoutComponent } from '@ui/main-layout/main-layout.component';
+import { TableComponent } from '@ui/table/table.component';
+import { Vendor } from 'app/prisma-types';
+import { map, startWith, Subject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-vendor',
   standalone: true,
-  imports: [],
+  imports: [
+    MainLayoutComponent,
+    MatButtonModule,
+    TableComponent,
+    MatIcon,
+    RouterLink,
+  ],
   templateUrl: './vendor.component.html',
-  styleUrl: './vendor.component.scss'
+  styleUrl: './vendor.component.scss',
 })
 export class VendorComponent {
+  private readonly _snackBar = inject(MatSnackBar);
+  private readonly _sanitizer = inject(DomSanitizer);
+  private readonly _vendorSrv = inject(VendorService);
 
+  data: Vendor[] = [];
+  isLoading: boolean = false;
+  fetchSubject = new Subject<void>();
+  columns: TTableColumnDef<Vendor>[] = [
+    {
+      columnDef: 'image',
+      header: 'Image',
+      cell: (row: Vendor) => {
+        const div = `<img src="${row.image || 'images/placeholder-image.jpg'}" class="size-8 rounded shadow object-cover" />`;
+        return this._sanitizer.bypassSecurityTrustHtml(div);
+      },
+    },
+    {
+      columnDef: 'name',
+      header: 'Vendor Name',
+      cell: (row: Vendor) => row.name,
+    },
+    {
+      columnDef: 'isActive',
+      header: 'Status',
+      cell: (row: Vendor) => (row.isActive ? 'Active' : 'Inactive'),
+    },
+  ];
+
+  ngOnInit() {
+    this.fetchSubject
+      .pipe(startWith({}))
+      .pipe(
+        switchMap(() => {
+          this.isLoading = true;
+          return this._vendorSrv.getVendorList();
+        }),
+        map((data) => {
+          console.log(data.data);
+          return data.data;
+        }),
+      )
+      .subscribe({
+        next: (data) => {
+          this.data = data;
+          this.isLoading = false;
+        },
+        error: () => {
+          this.isLoading = false;
+        },
+      });
+  }
+
+  onToggleStatusClick(id: string) {
+    this._vendorSrv.toggleVendorStatus(id).subscribe({
+      next: (data) => {
+        if (data.isSuccess) {
+          this.fetchSubject.next();
+        }
+        this._snackBar.open(data.message, 'Close');
+      },
+      error: () => {
+        this._snackBar.open('Failed to update vendor status', 'Close', {
+          duration: 3000,
+        });
+      },
+    });
+  }
 }
--- src/app/services/vendor/vendor.service.ts
+++ src/app/services/vendor/vendor.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@env/environment';
+import { TBaseResponse } from '@models/index';
+import { Vendor } from 'app/prisma-types';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class VendorService {
+  private readonly _vendorUrl = 'vendor';
 
-  constructor() { }
+  private readonly _http = inject(HttpClient);
+
+  getVendorList(): Observable<TBaseResponse<Vendor[]>> {
+    const url = environment.BASE_URL + this._vendorUrl;
+    return this._http.get<TBaseResponse<Vendor[]>>(url);
+  }
+
+  getVendorDetails(id: string): Observable<TBaseResponse<Vendor>> {
+    const url = environment.BASE_URL + this._vendorUrl + '/' + id;
+    return this._http.get<TBaseResponse<Vendor>>(url);
+  }
+
+  createVendor(request: Vendor): Observable<TBaseResponse<Vendor>> {
+    const url = environment.BASE_URL + this._vendorUrl;
+    return this._http.post<TBaseResponse<Vendor>>(url, request);
+  }
+
+  editVendorDetails(
+    id: string,
+    request: Vendor,
+  ): Observable<TBaseResponse<Vendor>> {
+    const url = environment.BASE_URL + this._vendorUrl + '/' + id;
+    return this._http.patch<TBaseResponse<Vendor>>(url, request);
+  }
+
+  toggleVendorStatus(id: string): Observable<TBaseResponse<Vendor>> {
+    const url =
+      environment.BASE_URL + this._vendorUrl + '/' + id + '/toggle-status';
+    return this._http.patch<TBaseResponse<Vendor>>(url, {});
+  }
 }
